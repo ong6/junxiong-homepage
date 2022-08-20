9be334882a24dd7d034e81808e701aa39184766a
ong6 <junxiongong2@gmail.com>
2022-08-21 01:58:02 +0200

    Minor ui touchups

--- components/Resume/ResumeDetail.js
+++ components/Resume/ResumeDetail.js
@@ -1,10 +1,21 @@
-import { Box, ListItem, Spacer, Text, UnorderedList } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	ListItem,
+	Spacer,
+	Text,
+	UnorderedList,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function ResumeDetail({ detail }) {
 	return (
 		<>
-			<Box display="flex" alignItems="center" justifyContent="space-between">
+			<Box
+				display="flex"
+				alignItems="center"
+				justifyContent="space-between"
+				flexDir={{ sm: "column", smmd: "row" }}>
 				<Text fontSize={18} fontWeight="bold">
 					{detail.title}
 				</Text>
@@ -13,25 +24,29 @@ export default function ResumeDetail({ detail }) {
 				</Text>
 			</Box>
 			{detail.roles.map((role, index) => (
-				<>
-					<Box
-						key={index}
-						display="flex"
+				<Box key={index}>
+					<Flex
 						alignItems="center"
-						justifyContent="space-between">
-						<Text fontSize={18} fontWeight="semibold" fontStyle="italic">
+						justifyContent="space-between"
+						flexDir={{ sm: "column", smmd: "row" }}
+						mb={{ sm: 4, smmd: 1 }}>
+						<Text
+							fontSize={18}
+							fontWeight="semibold"
+							fontStyle="italic"
+							textAlign={{ sm: "center", smmd: "left" }}>
 							{role.subtitle}
 						</Text>
 						<Text fontSize={18} fontWeight="semibold">
 							{role.date}
 						</Text>
-					</Box>
+					</Flex>
 					<UnorderedList pl={8} fontSize={18} fontWeight="medium" mb={2}>
 						{role.description.map((description, index) => (
 							<ListItem key={index}>{description}</ListItem>
 						))}
 					</UnorderedList>
-				</>
+				</Box>
 			))}
 			<Spacer pb={2} />
 			{detail.tech}
